51edd316cb31776798206dd96cb9ace67c4286a6
abu-amar jumhuur <jumhuur123@hotmail.com>
2023-06-27 09:09:38 +0300

    remove context

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,6 @@ import "./css/all.min.css";
 import { Roboto } from "next/font/google";
 import Footer from "@/components/Footer";
 import AuthProvider from "@/components/authProvider";
-import { ContextProvider } from "@/context/context";
 const inter = Roboto({ subsets: ["latin"], weight: ["400"] });
 
 export const metadata = {
@@ -16,13 +15,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ContextProvider>
-          <AuthProvider>
-            <Nav />
-            {children}
-            <Footer />
-          </AuthProvider>
-        </ContextProvider>
+        <AuthProvider>
+          <Nav />
+          {children}
+          <Footer />
+        </AuthProvider>
       </body>
     </html>
   );
