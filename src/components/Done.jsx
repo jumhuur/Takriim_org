0592fbb5c93277697cff53c3b6bcd3c5ec661250
abu-amar jumhuur <jumhuur123@hotmail.com>
2023-07-02 21:44:28 +0300

    sound animation dabaaldeg

--- src/components/Done.jsx
+++ src/components/Done.jsx
@@ -2,7 +2,15 @@
 import Image from "next/image";
 import Link from "next/link";
 import Lottie from "lottie-react";
-import animationData from "../../public/images/112910-confetti.json";
+import animationData from "../../public/images/123026-celebrations-begin.json";
+import animationData2 from "../../public/images/143796-blue-confetti.json";
+import animationData3 from "../../public/images/97818-celebration-animation.json";
+import animationData4 from "../../public/images/112910-confetti.json";
+import animationData5 from "../../public/images/116495-thank-you-without-text.json";
+//97818-celebration-animation 1aad
+//123026-celebrations-begin   2aad
+//112910-confetti
+//143796-blue-confetti
 // import Farxad_soud from "../../public/Media/farxad.mp3";
 import { useEffect, useRef } from "react";
 const Done = ({ Total }) => {
@@ -18,8 +26,12 @@ const Done = ({ Total }) => {
         <div className="inhaye">
           <div className="login  Done">
             <div className="llc bidix_login comm">
-              {/* <Image fill={true} src="/Images/comm.svg" alt="Login_img" /> */}
-              <Lottie animationData={animationData} />
+              <section className="animation">
+                <Lottie animationData={animationData} />
+                <Lottie animationData={animationData2} />
+                <Lottie animationData={animationData3} />
+                <Lottie animationData={animationData4} />
+              </section>
               <audio ref={code} src="/Media/farxad.mp3" />
               <audio ref={Shukran} src="/Media/Shukran.mp3" />
               {/* <Lottie animationData={animationData} /> */}
