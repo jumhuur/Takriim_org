5b4b47ae31bc7b1b22bc345d0dd8e1954887877b
abu-amar jumhuur <jumhuur123@hotmail.com>
2023-07-03 22:38:30 +0300

    online update

--- src/app/mashruuc/[Id]/page.jsx
+++ src/app/mashruuc/[Id]/page.jsx
@@ -7,9 +7,9 @@ import useSWR from "swr";
 import Comming from "@/components/Comming";
 import Done from "@/components/Done";
 async function getData(Id) {
-  const res = await fetch(`http://127.0.0.1:3000/api/projects/${Id}`, {
-    // next: { revalidate: 10 },
-    cache: "no-store",
+  const res = await fetch(`https://tabaruc.vercel.app/api/projects/${Id}`, {
+    next: { revalidate: 1 },
+    // cache: "no-store",
   });
 
   if (!res.ok) {
@@ -20,10 +20,13 @@ async function getData(Id) {
 }
 
 async function GetTotal(Id) {
-  const res = await fetch(`http://127.0.0.1:3000/api/Tabaruc/getTotal/${Id}`, {
-    // next: { revalidate: 10 },
-    cache: "no-store",
-  });
+  const res = await fetch(
+    `https://tabaruc.vercel.app/api/Tabaruc/getTotal/${Id}`,
+    {
+      next: { revalidate: 1 },
+      // cache: "no-store",
+    }
+  );
 
   return res.json();
 }
